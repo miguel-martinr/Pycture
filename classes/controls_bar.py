97c48de6c96183774da4c535e9587bd098a17e90
Miguel Martín <alu0101209777@ull.edu.es>
2021-10-06 02:47:42 +0100

    Edit menu added

--- classes/controls_bar.py
+++ classes/controls_bar.py
@@ -1,18 +1,20 @@
 from PyQt5.QtWidgets import QGridLayout, QMenuBar, QWidget
-from classes.cmd_menus import FileMenu
-from classes.commands import OpenFile
+from classes.cmd_menus import menus
+
 
 
 class ControlsBar(QMenuBar):
     def __init__(self, parent: QWidget, layout: QGridLayout) -> None:
         super().__init__(parent)  
-        self.parent = parent
+        
         self.setUI(layout)
         
 
     def setUI(self, layout):
         
         layout.addWidget(self, 0, 0)
-        fileMenu = FileMenu(self)
-        self.addMenu(fileMenu)
+        for menu in menus:
+          someMenu = menu(self)
+          self.addMenu(someMenu)
+        
 
