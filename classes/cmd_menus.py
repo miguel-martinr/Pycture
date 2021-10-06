bd15a74b2ed97d9bbc26225808a62b1180f09f1d
Miguel Martín <alu0101209777@ull.edu.es>
2021-10-06 03:25:44 +0100

    CmdMenu color modif deleted

--- classes/cmd_menus.py
+++ classes/cmd_menus.py
@@ -5,7 +5,7 @@ from classes.commands import Command, file_cmds, edit_cmds
 class CmdMenu(QMenu):
   def __init__(self, parent: QWidget):
     super(QMenu, self).__init__(parent)
-    self.setStyleSheet("background-color: gray;")
+
     
 
 
