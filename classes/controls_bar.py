1f7c5dd4c48818874158e70b3206a3b123c542ac
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-10-07 14:27:22 +0100

    Properly use QMainWindow

--- classes/controls_bar.py
+++ classes/controls_bar.py
@@ -1,19 +1,18 @@
 from PyQt5.QtGui import QColorConstants, QPalette
-from PyQt5.QtWidgets import QGridLayout, QMenuBar, QWidget
+from PyQt5.QtWidgets import QGridLayout, QMenuBar, QWidget, QHBoxLayout
 from classes.cmd_menus import menus
 
 
 
 class ControlsBar(QMenuBar):
-    def __init__(self, parent: QWidget, layout: QGridLayout) -> None:
+    def __init__(self, parent: QWidget) -> None:
         super().__init__(parent)  
-        self.setUI(layout)
-        
-
-    def setUI(self, layout):   
         # self.setMaximumSize(200, 50)
         self.setStyleSheet("background-color: darkgray")
-        layout.addWidget(self, 0, 0)
+        self.setup_menus()
+        
+
+    def setup_menus(self):
         for menu in menus:
           someMenu = menu(self)
           self.addMenu(someMenu)
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-from PyQt5.QtWidgets import QApplication, QGridLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from classes.controls_bar import ControlsBar
+from sys import argv
 
-app = QApplication([])
-window = QWidget()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Pycture")
+        self.setMenuBar(ControlsBar(self))
+        self.setGeometry(100,100,100,100)
 
-layout = QGridLayout()
-window.setLayout(layout)
-controlBar = ControlsBar(window, layout)
-window.setWindowTitle("Pycture")
-
+app = QApplication(argv)
+window = MainWindow()
 window.show()
 app.exec()
