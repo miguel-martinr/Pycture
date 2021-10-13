44a817251ea967e41bd74f944e5fafae2a803ef7
Miguel Martín <alu0101209777@ull.edu.es>
2021-10-13 01:02:31 +0100

    gray hist possibly calculated fine

--- src/pycture/editor/image.py
+++ src/pycture/editor/image.py
@@ -8,7 +8,12 @@ class Color(Enum):
     Red = 0
     Green = 1
     Blue = 2
-
+ 
+    
+GrayScale = {
+    "NTSC" : [0.299, 0.587, 0.114],
+    "Pal" : [0.222, 0.707, 0.071]
+}
 class Image(QLabel):
     def __init__(self, parent: QWidget, image: QPixmap):
         super().__init__(parent)
@@ -17,16 +22,21 @@ class Image(QLabel):
         
     def setup_histogram_data(self) -> List[float]:
         image = self.pixmap().toImage()
-        histograms = [[0] * 256, [0] * 256, [0] * 256]
-        means = [0] * 3
+        histograms = [[0] * 256, [0] * 256, [0] * 256, [0] * 256]
+        means = [0] * 4
         for x in range(image.width()):
           for y in range(image.height()):
+            gray_value = 0
             pixel = image.pixel(x, y)
             get_value = [self.get_red_value, self.get_green_value, self.get_blue_value]
             for color in Color:
                 value = get_value[color.value](pixel)
                 histograms[color.value][value] += 1
                 means[color.value] += value
+                gray_value += value * GrayScale["NTSC"][color.value] # PAL available
+            gray_value = int(gray_value)  # int(gray_value) or a different rounding?
+            histograms[3][gray_value] += 1
+            means[3] += 1        
 
         total_pixels = image.width() * image.height()
         self.histograms = list(map(lambda histogram:
@@ -34,6 +44,8 @@ class Image(QLabel):
             histograms
         ))
         self.means = list(map(lambda mean: mean / total_pixels, means))
+        
+          
 
     def get_red_value(self, pixel):
         return (pixel & 0x00ff0000) >> 16
@@ -46,6 +58,8 @@ class Image(QLabel):
 
     def get_histogram(self, color: Color):
         return self.histograms[color.value]
+
+    
 
     def get_mean(self, color: Color):
         return self.means[color.value]
