3c59570f19028ea147e0645ce7279a88d71281ae
Miguel Martín <alu0101209777@ull.edu.es>
2021-10-15 13:11:21 +0100

    Image: info methods added

--- src/pycture/editor/image.py
+++ src/pycture/editor/image.py
@@ -5,17 +5,20 @@ from PyQt5.QtWidgets import QLabel, QWidget
 from PyQt5.QtGui import QPixmap, QMouseEvent
 from PyQt5.QtCore import Qt
 
+
 class Color(Enum):
     Red = 0
     Green = 1
     Blue = 2
     Gray = 3
- 
+
+
 # LUT stands for LookUpTable
 # These gray scale transformation values correspond to the NTSC method
 GrayScaleLUT = [list(map(lambda r: 0.299 * r, list(range(256)))),
                 list(map(lambda r: 0.587 * r, list(range(256)))),
                 list(map(lambda r: 0.114 * r, list(range(256))))]
+
 
 class Image(QLabel):
     def __init__(self, parent: QWidget, image: QPixmap):
@@ -28,20 +31,32 @@ class Image(QLabel):
         self.setAlignment(Qt.AlignLeft | Qt.AlignTop)
         self.setMaximumHeight(image.height())
         self.setMaximumWidth(image.width())
-        
+
     def setup_info(self):
         pixmap = self.pixmap()
         self.info = {
-            "width": pixmap.width(),
-            "heigth": pixmap.height(),
-            "ranges": self.ranges,
-            "brightness": self.means
+            "width": self.get_width(),
+            "heigth": self.height(),
+            "ranges": self.get_ranges(),
+            "brightness": list(map(lambda color: self.get_mean(color), Color)),
         }
         print(self.info)
 
+    def get_width(self):
+        return self.pixmap().width()
+
+    def get_height(self):
+        return self.pixmap().height()
+
+    def get_ranges(self):
+        """Returns a list with RGB-GrayScale ranges (in that order): 
+             R            G           B         Gray scale
+        [[min, max], [min, max], [min, max], [min, max]]
+        """
+        return self.ranges
+
     def get_info(self):
         return self.info
-
 
     def setup_histogram_data(self) -> List[float]:
         image = self.pixmap().toImage()
@@ -52,7 +67,8 @@ class Image(QLabel):
             for y in range(image.height()):
                 gray_value = 0
                 pixel = image.pixel(x, y)
-                get_value = [self.get_red_value, self.get_green_value, self.get_blue_value]
+                get_value = [self.get_red_value,
+                             self.get_green_value, self.get_blue_value]
                 for color in [Color.Red, Color.Green, Color.Blue]:
                     value = get_value[color.value](pixel)
                     histograms[color.value][value] += 1
@@ -71,34 +87,34 @@ class Image(QLabel):
                     self.ranges[Color.Gray.value][0] = value
                 if (value > self.ranges[Color.Gray.value][1]):
                     self.ranges[Color.Gray.value][1] = value
-                
+
                 # GrayScale histogram and mean
                 histograms[Color.Gray.value][gray_value] += 1
-                means[Color.Gray.value] += gray_value   
+                means[Color.Gray.value] += gray_value
 
         total_pixels = image.width() * image.height()
         self.histograms = list(map(lambda histogram:
-            list(map(lambda x: x / total_pixels, histogram)),
-            histograms
-        ))
+                                   list(map(lambda x: x / total_pixels, histogram)),
+                                   histograms
+                                   ))
         self.means = list(map(lambda mean: mean / total_pixels, means))
-        
+
     def get_red_value(self, pixel):
         return (pixel & 0x00ff0000) >> 16
-    
+
     def get_green_value(self, pixel):
         return (pixel & 0x0000ff00) >> 8
 
     def get_blue_value(self, pixel):
-        return pixel & 0x000000ff 
+        return pixel & 0x000000ff
 
     def get_histogram(self, color: Color):
-        if (color == 3): # Gray scale temp fix
+        if (color == 3):  # Gray scale temp fix
             return self.histograms[3]
         return self.histograms[color.value]
 
     def get_mean(self, color: Color):
-      return self.means[color.value]
+        return self.means[color.value]
 
     def get_gray_scaled_image(self):
         image = self.pixmap().toImage()
@@ -110,10 +126,13 @@ class Image(QLabel):
         for x in range(width):
             for y in range(heigth):
                 color_value = image.pixel(x, y)
-                red_comp = GrayScaleLUT[Color.Red.value][self.get_red_value(color_value)]
-                green_comp = GrayScaleLUT[Color.Green.value][self.get_green_value(color_value)]
-                blue_comp = GrayScaleLUT[Color.Blue.value][self.get_blue_value(color_value)]
-              
+                red_comp = GrayScaleLUT[Color.Red.value][self.get_red_value(
+                    color_value)]
+                green_comp = GrayScaleLUT[Color.Green.value][self.get_green_value(
+                    color_value)]
+                blue_comp = GrayScaleLUT[Color.Blue.value][self.get_blue_value(
+                    color_value)]
+
                 gray_value = round(red_comp + green_comp + blue_comp)
                 for _ in range(2):
                     gray_value = gray_value | (gray_value << 8)
@@ -122,7 +141,7 @@ class Image(QLabel):
                 gray_value = gray_value | (color_value & 0xff000000)
                 gray_scaled.setPixel(x, y, gray_value)
 
-        return gray_scaled 
+        return gray_scaled
 
     def mouseMoveEvent(self, event: QMouseEvent):
         x = event.x()
@@ -135,4 +154,3 @@ class Image(QLabel):
         green_val = self.get_green_value(pixel_val)
         blue_val = self.get_blue_value(pixel_val)
         self.parent().data_bar.update_color((red_val, green_val, blue_val))
-
