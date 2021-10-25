94e7dfd7e540c6adb67c37dbb68199f1b228b552
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-10-25 09:00:09 +0100

    Add help menu

--- src/pycture/commands/__init__.py
+++ src/pycture/commands/__init__.py
@@ -3,6 +3,7 @@ from .file_commands import OpenFileCommand, SaveFileCommand
 from .view_commands import (ViewRedHistogram, ViewGreenHistogram, ViewBlueHistogram,
     ViewGrayScaleHistogram, ViewImageBrightness, ViewImageSize, ViewImageContrast,
     ViewImageEntropy, ViewImageRanges)
+from .help_commands import HelpCommand
 from .command import Command
 
 
@@ -10,3 +11,4 @@ file_command_list = [OpenFileCommand, SaveFileCommand]
 edit_command_list = [EditBrightnessCommand]
 histogram_command_list = [ViewRedHistogram, ViewGreenHistogram, ViewBlueHistogram, ViewGrayScaleHistogram]
 info_command_list = [ViewImageBrightness, ViewImageSize, ViewImageContrast, ViewImageEntropy, ViewImageRanges]
+help_command_list = [HelpCommand]
