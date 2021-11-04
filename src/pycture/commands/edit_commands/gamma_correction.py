70bb740ffc48ec07d9042e360297436fc99635a7
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-04 22:07:20 +0000

    gamma correction working

--- src/pycture/commands/edit_commands/gamma_correction.py
+++ src/pycture/commands/edit_commands/gamma_correction.py
@@ -12,12 +12,9 @@ class GammaCorrection(Command):
         b = a ** gamma_value
         return b
 
-    def execute(self, main_window: QMainWindow):
+    def apply(self, gamma_value, main_window):
         active_image, title = self.get_active_image_and_title(main_window)
         if (not active_image): return
-
-        gamma_value = 5
-        dialog = GammaCorrectionDialog(main_window)
 
         # map a (Vin) in [0, 1]
         A = [a / 255 for a in range(256)]
@@ -28,5 +25,13 @@ class GammaCorrection(Command):
         # map b (Vout) in [0, 255]
         luts = [[round(b * 255) for b in B]] * 3
         new_image = active_image.apply_LUTs(luts)
-        main_window.add_editor(new_image, title + " (Gamma corrected)")
+        main_window.add_editor(new_image, title + f" (Gamma corrected - {gamma_value})")
+
+    def execute(self, main_window: QMainWindow):
+        active_image, title = self.get_active_image_and_title(main_window)
+        if (not active_image): return
+
+        dialog = GammaCorrectionDialog(main_window)
+        dialog.applied.connect(lambda gamma_value: self.apply(gamma_value, main_window))
+
         
