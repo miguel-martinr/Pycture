cbf0ade238b469ef320404a0d75286c9ea3ebe12
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-04 22:01:03 +0000

    apply button added

--- src/pycture/dialogs/gamma_correction_dialog.py
+++ src/pycture/dialogs/gamma_correction_dialog.py
@@ -1,15 +1,20 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, Signal
 from PyQt5.QtGui import QDoubleValidator, QValidator
-from PyQt5.QtWidgets import QDialog, QGridLayout, QLineEdit, QMainWindow, QSlider
+from PyQt5.QtWidgets import QDialog, QGridLayout, QLineEdit, QMainWindow, QPushButton, QSlider
 import re
 
 class GammaCorrectionDialog(QDialog):
+    
+    applied = Signal(float)
+    
     def __init__(self, parent: QMainWindow, top = 20) -> None:
         super().__init__(parent, Qt.WindowType.Window)
         self._setup_(top)
         self.show()
 
+
     def _setup_(self, top):
+        self.setFixedWidth(300)
         layout = QGridLayout()
         self.setLayout(layout)
 
@@ -21,22 +26,32 @@ class GammaCorrectionDialog(QDialog):
         layout = QGridLayout()
         self.layout().addLayout(layout, 0, 0)
     
-        self.slider = QSlider(Qt.Orientation.Horizontal, self)
-        self.slider.setMaximum(top / 0.02)
-        layout.addWidget(self.slider, 0, 0, Qt.AlignmentFlag.AlignCenter)
+        self._slider_ = QSlider(Qt.Orientation.Horizontal, self)
+        self._slider_.setMaximum(top / 0.02)
+        layout.addWidget(self._slider_, 0, 0, Qt.AlignmentFlag.AlignCenter)
 
-        self.numeric_input = QLineEdit("0.5", self)
-        self.numeric_input.setFixedWidth(40)
-        self.numeric_input.setValidator(DoubleValidator(0, top, 2))
-        layout.addWidget(self.numeric_input, 0, 1, Qt.AlignmentFlag.AlignRight)
+        self._numeric_input_ = QLineEdit("0.5", self)
+        self._numeric_input_.setFixedWidth(40)
+        self._numeric_input_.setValidator(DoubleValidator(0, top, 2))
+        layout.addWidget(self._numeric_input_, 0, 1, Qt.AlignmentFlag.AlignCenter)
         
-        to_double = lambda text: 0.0 if text == "" else float(text)
-        self.slider.sliderMoved.connect(lambda value: self.numeric_input.setText(f"{(value * 0.02):.2f}"))
-        self.numeric_input.textEdited.connect(lambda text: self.slider.setValue(round(to_double(text) / 0.02)))
+        
+        self._slider_.sliderMoved.connect(lambda value: self._numeric_input_.setText(f"{(value * 0.02):.2f}"))
+        self._numeric_input_.textEdited.connect(lambda text: self._slider_.setValue(round(self._to_double_(text) / 0.02)))
 
     def _set_buttons_(self):
-        pass
+        layout = QGridLayout()
+        self.layout().addLayout(layout, 1, 0)
 
+        accept_btn = QPushButton("Apply", self)
+        accept_btn.clicked.connect(lambda: self.applied.emit(self.get_gamma()))
+        layout.addWidget(accept_btn, 0, 0, Qt.AlignmentFlag.AlignCenter)
+
+    def get_gamma(self):
+        return self._to_double_(self._numeric_input_.text())
+
+    def _to_double_(self, text):
+        return 0.0 if text == "" else float(text)
 
 class DoubleValidator(QDoubleValidator):
     def __init__(self, bottom: int, top: int, decimals: int):
