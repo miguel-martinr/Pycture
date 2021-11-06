5da5b556a047d11a74ea80b2f232c8c9050c9059
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-11-06 15:06:26 +0000

    Add first draft of specification
    It seems there is some kind of bug though

--- src/pycture/commands/edit_commands/specify_histogram.py
+++ src/pycture/commands/edit_commands/specify_histogram.py
@@ -1,7 +1,10 @@
+from typing import List
+
 from PyQt5.QtWidgets import QWidget, QMainWindow
 
 from ..command import Command
 from pycture.dialogs import HistogramSpecificationDialog
+from pycture.editor.image import RGBColor
 
 
 class SpecifyHistogram(Command):
@@ -18,5 +21,27 @@ class SpecifyHistogram(Command):
 
     def specify_histogram(self, base: str, sample: str,
                           rgb: (bool, bool, bool)):
-        new_image = self.main_window.get_editor(base).get_image()
+        base_image = self.main_window.get_editor(base).get_image()
+        sample_image = self.main_window.get_editor(sample).get_image()
+
+        luts = [list(range(256)) for x in range(3)]
+        for color in RGBColor:
+            if not rgb[color.value]:
+                continue
+            lut = luts[color.value]
+            base_histogram = base_image.get_cumulative_histogram(color)
+            sample_histogram = sample_image.get_cumulative_histogram(color)
+            self.setup_lut(lut, base_histogram, sample_histogram)
+        new_image = base_image.apply_LUTs(luts)
+
         self.main_window.add_editor(new_image, base + "(ModHist)")
+
+    def setup_lut(self, lut: List[int], base: List[float], sample: List[float]):
+        sample_index = 0
+        for index, val in enumerate(base):
+            while (sample_index < 255 and base[index] > sample[sample_index]):
+                sample_index += 1
+            if (sample_index < 255 and sample[sample_index] - base[index] >
+                base[index] - sample[sample_index]):
+                sample_index += 1
+            lut[index] = sample_index
