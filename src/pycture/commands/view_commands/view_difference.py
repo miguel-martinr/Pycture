15178d72ed55facbac039ac228d0e351eb3cda6f
Miguel Martín <alu0101209777@ull.edu.es>
2021-11-07 19:58:54 +0000

    some crashes fixed

--- src/pycture/commands/view_commands/view_difference.py
+++ src/pycture/commands/view_commands/view_difference.py
@@ -15,17 +15,19 @@ class ViewDifference(Command):
     def __init__(self, parent: QtWidgets):
         self.active_histogram = None
         self.map_dialog = None
+        self.difference_editor = None
         super().__init__(parent, "Difference")
 
     def _show_difference_(self, image_a_title, image_b_title):
-        self.image_a_title = image_a_title
-        self.image_b_title = image_b_title
-        editor_a = self.main_window.get_editor(image_a_title)
-        editor_b = self.main_window.get_editor(image_b_title)
+
+        self.image_a_editor = editor_a = self.main_window.get_editor(
+            image_a_title)
+
+        self.image_b_editor = editor_b = self.main_window.get_editor(
+            image_b_title)
 
         image_a = editor_a.get_image()
         image_b = editor_b.get_image()
-        self.image_a = image_a
 
         if (image_a.height() != image_b.height() or image_a.width() != image_b.width()):
             Notification(
@@ -33,14 +35,11 @@ class ViewDifference(Command):
             return False
 
         difference = image_a.get_difference(image_b)
+        title = f" -  diff({image_a_title}, {image_b_title})"
         self.main_window.add_editor(
-            difference, f" -  diff({image_a_title}, {image_b_title})")
-        self.difference = self.main_window.get_active_editor().get_image()
-        if (self.map_dialog):
-            self.difference.worker.finished.connect(
-                lambda: self.map_dialog.rgb_plane_changed.emit(0))
-
-        return (image_a, difference)
+            difference, title)
+        self.difference_editor = self.main_window.get_editor(title)
+        return True
 
     def _update_histogram_view_(self, color_index: int):
         if (color_index < 0):
@@ -49,27 +48,37 @@ class ViewDifference(Command):
             self.active_histogram.deleteLater()
         self.active_histogram = self.histograms[color_index](self.main_window)
 
+        self.main_window.set_active_editor(
+            self.difference_editor.windowTitle())
+
         self.active_histogram.execute(self.main_window)
 
     def mark_map_of_changes(self, treshold: int, rgb_plane: RGBColor, marker_color: QColor):
-        marked_pixels_coordinates = self.difference.get_pixels_coordinates(treshold, rgb_plane)
-        map_of_changes = self.image_a.mark_pixels(
+        marked_pixels_coordinates = self.difference_editor.get_image(
+        ).get_pixels_coordinates(treshold, rgb_plane)
+        map_of_changes = self.image_a_editor.get_image().mark_pixels(
             marked_pixels_coordinates, marker_color)
         self.main_window.add_editor(
-            map_of_changes, f"Map of changes ({self.image_a_title} - {self.image_b_title})")
+            map_of_changes, f"Map of changes ({self.image_a_editor.windowTitle()} - {self.image_b_editor.windowTitle()})")
 
     def _trigger_map_of_changes_(self, image_a_title, image_b_title):
         self.map_dialog = MapOfChangesDialog(self.main_window)
-        difference_result = self._show_difference_(
-            image_a_title, image_b_title)
-        if (not difference_result):
-            return
-        
-        self.map_dialog.show()
-        image_a, difference = difference_result
-
+        if (not self.difference_editor):
+            difference_showed = self._show_difference_(
+                image_a_title, image_b_title)
+            if (not difference_showed):
+                return
         self.map_dialog.rgb_plane_changed.connect(
             lambda color_index: self._update_histogram_view_(color_index))
+
+        if (self.difference_editor.get_image().load_finished):
+            self.map_dialog.rgb_plane_changed.emit(0)
+        else:
+            self.difference_editor.get_image().worker.finished.connect(
+                lambda: self.map_dialog.rgb_plane_changed.emit(0))
+
+        self.map_dialog.show()
+
         self.map_dialog.create_map.connect(self.mark_map_of_changes)
 
     def execute(self, main_window: QtWidgets.QMainWindow):
