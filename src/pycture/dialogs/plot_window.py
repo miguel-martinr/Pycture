1b3eca1c788fabae2e4af77aa27b3a0f07c3744d
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-11-15 19:39:47 +0000

    Correctly handle histogram plt figures

--- src/pycture/dialogs/plot_window.py
+++ src/pycture/dialogs/plot_window.py
@@ -1,14 +1,17 @@
 from PyQt5.QtWidgets import QVBoxLayout, QWidget 
 from PyQt5.QtCore import Qt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 
 from pycture.css import PLOT_TOOLBAR_CSS
 
 class PlotWindow(QWidget):
-    def __init__(self, parent: QWidget, plot: FigureCanvasQTAgg, title: str):
+    def __init__(self, parent: QWidget, figure: Figure, title: str):
         super().__init__(parent)
         self.setWindowTitle(title)
         self.layout = QVBoxLayout(self)
+        plot = FigureCanvasQTAgg(figure)
         toolbar = NavigationToolbar2QT(plot, self)
         toolbar.setStyleSheet(PLOT_TOOLBAR_CSS)
         self.layout.addWidget(toolbar)
@@ -17,3 +20,7 @@ class PlotWindow(QWidget):
         self.setParent(parent, Qt.WindowType.Window)
         self.setMinimumWidth(300)
         self.show()
+
+    def destroy(self, a, b):
+        plt.close(figure)
+        super().destroy(a, b)
