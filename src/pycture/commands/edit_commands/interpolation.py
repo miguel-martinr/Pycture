9cf0efed1389a1a10371639bf09ee96942b0fa9a
Miguel Martín <alu0101209777@ull.edu.es>
2021-12-25 16:42:18 +0000

    nearest neighbor interpolation added

---
+++ src/pycture/commands/edit_commands/interpolation.py
@@ -0,0 +1,22 @@
+
+from PyQt5.QtGui import QImage
+from math import floor, sqrt
+
+def nearest_neighbor_interpolation(image: QImage, point: tuple):
+    _x, _y = point
+
+    X = floor(_x)
+    X_one = min(X + 1, image.width() - 1)
+
+    Y = floor(_y)
+    Y_one = min(Y + 1, image.height() - 1)
+
+    #           top left     top right       bottom left     bottom right
+    corners = ((X, Y_one),   (X_one, Y_one),  (X, Y),         (X_one, Y))
+
+    distances = [sqrt((x - _x)**2 + (y - _y)**2)
+                 for x, y in corners]
+    nearest_neighbor = corners[distances.index(min(distances))]
+    return image.pixel(nearest_neighbor[0], nearest_neighbor[1])
+    
+    
--- src/pycture/commands/edit_commands/rotate.py
+++ src/pycture/commands/edit_commands/rotate.py
@@ -1,23 +1,28 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QImage
 from pycture.commands.command import Command
+from pycture.commands.edit_commands.interpolation import nearest_neighbor_interpolation
 from pycture.editor import Editor
 from pycture.editor.image import Image
-from math import ceil, cos, floor, sin, pi
+from math import ceil, cos, floor, sin, pi, sqrt
 import numpy as np
 
 
 class Rotate(Command):
     def __init__(self, parent: QtWidgets):
         super().__init__(parent, "Rotate")
+        self.interpolation_techniques = {
+            "Nearest Neighbour": nearest_neighbor_interpolation,
+        }
+
 
     def rotation_matrix(self, angle_rad: float):
         return np.array(
             ((cos(angle_rad), -sin(angle_rad)),
-            (sin(angle_rad), cos(angle_rad)))
+             (sin(angle_rad), cos(angle_rad)))
         )
 
-    def rotate(self, image: Image, angle_deg: float):
+    def rotate(self, image: Image, angle_deg: float, interpolation_technique):
         angle_rad = angle_deg * (pi / 180)
         old_top_left = (0, 0)
         old_top_right = (image.width() - 1, 0)
@@ -27,7 +32,8 @@ class Rotate(Command):
         dt_rotation_matrix = self.rotation_matrix(angle_rad)
         it_rotation_matrix = self.rotation_matrix(-angle_rad)
 
-        new_top_left = np.array([floor(value) for value in np.dot(dt_rotation_matrix, old_top_left)]) 
+        new_top_left = np.array(
+            [floor(value) for value in np.dot(dt_rotation_matrix, old_top_left)])
         new_top_right = np.dot(dt_rotation_matrix, old_top_right)
         new_bottom_left = np.dot(dt_rotation_matrix, old_bottom_left)
         new_bottom_right = np.dot(dt_rotation_matrix, old_bottom_right)
@@ -48,10 +54,11 @@ class Rotate(Command):
         new_image = QImage(new_width, new_height, image.format())
         for indexXp in range(new_width):
             for indexYp in range(new_height):
-                xp, yp = [floor(value) for value in (indexXp + min_x, indexYp + min_y)]
-                (x, y) = [floor(value) for value in np.dot(it_rotation_matrix, (xp, yp))]
+                xp, yp = (indexXp + min_x, indexYp + min_y)
+                x, y = np.dot(it_rotation_matrix, (xp, yp))
+
                 if (0 <= x < image.width() and 0 <= y < image.height()):
-                    new_image.setPixel(indexXp, indexYp, image.pixel(floor(x), floor(y)))
+                    new_image.setPixel(indexXp, indexYp, interpolation_technique(image, (x, y)))
                 else:
                     new_image.setPixel(indexXp, indexYp, 0xffffff)
         return new_image
@@ -59,8 +66,9 @@ class Rotate(Command):
     def execute(self, main_window: QtWidgets.QMainWindow):
         # Open dialog
         # Connect dialog button to rotate function
-        
+
         image, title = self.get_active_image_and_title(main_window)
-        rotated_image = self.rotate(image, -90)
-        main_window.add_editor(editor=Editor(main_window, rotated_image, title + ' rotated 45º'))
         
+        rotated_image = self.rotate(image, 211, self.interpolation_techniques['Nearest Neighbour'])
+        main_window.add_editor(editor=Editor(
+            main_window, rotated_image, title + ' rotated 45º'))
