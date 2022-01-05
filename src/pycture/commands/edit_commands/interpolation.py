c7306126626a04be6cd5d0869a09020cca3c4bfa
Miguel Martín <alu0101209777@ull.edu.es>
2022-01-05 21:48:23 +0000

    bilinear method added

--- src/pycture/commands/edit_commands/interpolation.py
+++ src/pycture/commands/edit_commands/interpolation.py
@@ -20,3 +20,27 @@ def nearest_neighbor_interpolation(image: QImage, point: (float, float)):
     return image.pixel(nearest_neighbor[0], nearest_neighbor[1])
     
     
+def bilinear_interpolation(image: QImage, point: (float, float)):
+    _x, _y = point
+
+    X = floor(_x)
+    X_one = min(X + 1, image.width() - 1)
+
+    Y = floor(_y)
+    Y_one = min(Y + 1, image.height() - 1)
+    
+    A = image.pixel(X, Y_one)      # top left
+    B = image.pixel(X_one, Y_one)  # top right
+    C = image.pixel(X, Y)          # bottom left
+    D = image.pixel(X_one, Y)      # bottom right
+    
+    p = _x - X
+    q = _y - Y
+    
+    Q = A + (B - A) * p
+    R = C + (D - C) * p
+    
+    P = R + (Q - R) * q
+    
+    return P
+    
