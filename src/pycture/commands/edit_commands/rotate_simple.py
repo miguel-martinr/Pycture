74c296468e80270984c47e65408e91e1dab009d4
Miguel Martín <alu0101209777@ull.edu.es>
2022-01-11 20:59:07 +0000

    RotateSimple command and dialog added

---
+++ src/pycture/commands/edit_commands/rotate_simple.py
@@ -0,0 +1,32 @@
+from PyQt5.QtWidgets import QWidget, QMainWindow
+from ..command import Command
+from .interpolation import bilinear_interpolation, nearest_neighbor_interpolation
+from pycture.dialogs import RotateDialog
+from pycture.editor import Editor
+
+
+class RotateSimple(Command):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent, "Rotate and paint")
+        
+    def apply_rotation(self, editor_title, angle):
+        editor = self.main_window.editors.get(editor_title)
+        image = editor.get_image()
+        title = editor.windowTitle()
+        
+        rotated_image = image.rotate_simple(angle)
+        
+        str_angle = str(angle).replace(".", "'")
+        self.main_window.add_editor(editor=Editor(
+            self.main_window, rotated_image, title + f' rotated {str_angle}º'))
+
+    def execute(self, main_window: QMainWindow):
+        # Open dialog
+        # Connect dialog button to rotate function
+        self.main_window = main_window
+        dialog = RotateSimpleDialog(main_window, main_window.get_editor_list())
+        dialog.set_editor(main_window.get_active_editor_name())
+        dialog.set_interpolation_technique(
+            list(self.interpolation_techniques.keys())[0])
+
+        dialog.applied.connect(self.apply_rotation)
---
+++ src/pycture/dialogs/rotate_simple_dialog.py
@@ -0,0 +1,57 @@
+from typing import List
+from PyQt5.QtWidgets import QDialog, QLabel, QLayout, QLineEdit, QMainWindow, QPushButton, QSizePolicy, QVBoxLayout, QWidget
+from PyQt5.QtCore import Qt, Signal
+from . import Notification
+
+from .widgets import CustomDoubleValidator, DropdownList
+
+
+class RotateSimpleDialog(QDialog):
+    #                img   angle
+    applied = Signal(str, float)
+
+    def __init__(self, parent: QMainWindow, editors: List[str], angle_limit = 180):
+        super().__init__(parent, Qt.WindowType.Window)
+        self.setWindowTitle("Scale")
+        self.layout = QVBoxLayout()
+        self.layout.setSizeConstraint(QLayout.SetFixedSize)
+        self.setLayout(self.layout)
+
+        self.setup(editors)
+
+        self.show()
+
+    def setup(self, editors: List[str]):
+        layout = QVBoxLayout()
+        self.layout.addLayout(layout)
+
+        self.editors_dropdown = DropdownList(self, editors)
+        layout.addWidget(self.editors_dropdown)
+
+        label = QLabel("Angle:", self)
+        layout.addWidget(label)
+
+        self.numeric_input = QLineEdit("1", self)
+        self.numeric_input.setValidator(
+            CustomDoubleValidator(-self.angle_limit, self.angle_limit, 2))
+        layout.addWidget(self.numeric_input)
+
+        separator = QWidget()
+        separator.setMinimumSize(0, 0)
+        separator.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        layout.addWidget(separator)
+
+        apply_button = QPushButton("Rotate", self)
+        apply_button.clicked.connect(self.emit_applied)
+        layout.addWidget(apply_button)
+
+    def emit_applied(self):
+        editor = self.editors_dropdown.currentText()
+        if self.parent().get_editor(editor) is None:
+            Notification(self, "An active image must be chosen")
+            return
+        self.applied.emit(self.editors_dropdown.currentText(),
+                          float(self.numeric_input.text()))
+
+    def set_editor(self, editor: str):
+        self.editors_dropdown.set_selected(editor)
